5b8c478e13d9a5888baa379881b4d2a8a6634547
Woodhouse91 <nskovh11@student.aau.dk>
2017-02-17 15:32:57 +0100

    project
    project commit

---
+++ Assets/findscreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class findscreen : MonoBehaviour {
+    public Transform screen;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = screen.position;
+        GetComponent<RectTransform>().sizeDelta = new Vector2(screen.localScale.x, screen.localScale.y);
+	}
+}
---
+++ Assets/followCAM.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followCAM : MonoBehaviour {
+
+    public Transform frontcam, screen;
+    private RectTransform can;
+    Quaternion look;
+    RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Physics.Raycast(frontcam.position, frontcam.forward,out hit, Mathf.Infinity,1<<LayerMask.NameToLayer("win")))
+            transform.position = hit.point;
+        transform.rotation = Quaternion.Euler(90, 0, 360-frontcam.rotation.eulerAngles.y);
+        transform.localScale = Vector3.one*(frontcam.position -transform.position).magnitude;
+	}
+}
