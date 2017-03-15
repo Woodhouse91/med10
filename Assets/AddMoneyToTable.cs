dadb6abacfc2e7db5b6162611d07ce9386e4edd5
KristofferLH <kristoffer_lh@hotmail.com>
2017-03-15 15:13:38 +0100

    coins added

---
+++ Assets/AddMoneyToTable.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMoneyToTable : MonoBehaviour {
+
+    Transform spawnArea;
+    public float Ax, Ay, Az;
+    public Vector3 centerpoint;
+    public GameObject Thousand, FiveHundred, TwoHundred, OneHundred, Fifty;
+    public GameObject Twenty, Ten, Five, Two, One;
+	// Use this for initialization
+	void Start () {
+        spawnArea = transform.GetChild(0);
+        Ax = spawnArea.lossyScale.x;
+        Ay = spawnArea.lossyScale.y;
+        Az = spawnArea.lossyScale.z;
+        centerpoint = spawnArea.localPosition;
+        int[,] myArray = {
+                  {3300, 2250, 10000},
+                  {50, 50, 50},
+                  {100, 100, 100},
+                  {30, 31, 32},
+              };
+        spawnAllTheMoney(myArray);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //insideArea(Thousand);
+	}
+    public void spawnAllTheMoney(int[,] arr)
+    {
+        int x = arr.GetLength(0);
+        int y = arr.GetLength(1);
+        for (int i = 0; i < x; i++)
+        {
+            for (int k = 0; k < y; k++)
+            {
+                if(arr[i, k] > 0)
+                    spawnMoney(arr[i, k]);
+
+            }
+        }
+    }
+    void spawnMoney(int money)
+    {
+        int restMoney = money;
+        ModulusMoney(restMoney, 1000, Thousand);
+        restMoney %= 1000;
+        ModulusMoney(restMoney, 500, FiveHundred);
+        restMoney %= 500;
+        ModulusMoney(restMoney, 200, TwoHundred);
+        restMoney %= 200;
+        ModulusMoney(restMoney, 100, OneHundred);
+        restMoney %= 100;
+        ModulusMoney(restMoney, 50, Fifty);
+        restMoney %= 50;
+        ModulusMoney(restMoney, 20, Twenty);
+        restMoney %= 20;
+        ModulusMoney(restMoney, 10, Ten);
+        restMoney %= 10;
+        ModulusMoney(restMoney, 5, Five);
+        restMoney %= 5;
+        ModulusMoney(restMoney, 2, Two);
+        restMoney %= 2;
+        ModulusMoney(restMoney, 1, One);
+    }
+    void ModulusMoney(int m, int bill, GameObject go)
+    {
+        for (int i = 0; i < m / bill; i++)
+        {
+            insideArea(go);
+        }
+    }
+
+    void insideArea(GameObject money)
+    {
+        Vector3 pos = new Vector3(Random.Range(-Ax / 2f, Ax / 2f), Random.Range(-Ay / 2f, Ay / 2f), Random.Range(-Az / 2f, Az / 2f)) + centerpoint;
+        pos = transform.TransformPoint(pos);
+        Instantiate(money, pos, Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up)* Quaternion.AngleAxis(Random.Range(0, 360), Vector3.forward));
+    }
+}
---
+++ Assets/Scripts/PlaceAllCrates.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class PlaceAllCrates : MonoBehaviour {
+
+    public GameObject[,] ListOfCrates;
+    public Vector3 SizeOfCrate; //should be (1.5f,1.0f,1.0f)
+    public int SizeOfBudget;
+    public int EXPANDDONGAT;
+    public float AngleRotate;
+    public GameObject Crate;
+	// Use this for initialization
+	void Start () {
+        ListOfCrates = new GameObject[14,SizeOfBudget];
+        PlaceThemAll(SizeOfBudget);
+        PlaceANewRow();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(ExpandRow(EXPANDDONGAT, 0.5f));
+        }
+	}
+    IEnumerator ExpandRow(int rowNumber, float percent)
+    {
+        for (int i = 1; i < 13; i++) // only going through crates
+        {
+            StartCoroutine(ExpandWithRow(ListOfCrates[i, rowNumber].transform.localScale, ListOfCrates[i, rowNumber].transform.localScale + Vector3.up * percent, ListOfCrates[i, rowNumber].transform));
+            for (int k = rowNumber-1; k >= 0; k--)
+            {
+                StartCoroutine(MoveWithRow(ListOfCrates[i, k].transform.position, ListOfCrates[i, k].transform.position + Vector3.up * SizeOfCrate.y * percent / 2f, ListOfCrates[i, k].transform));
+            }
+        }
+        yield return null;
+    }
+    IEnumerator MoveWithRow(Vector3 start, Vector3 end,Transform crate)
+    {
+        float t = 0;
+        while(t<1f)
+        {
+            crate.position = Vector3.Lerp(start, end, t);
+            t += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+    }
+    IEnumerator ExpandWithRow(Vector3 start, Vector3 end, Transform crate)
+    {
+        float t = 0;
+        while (t < 1f)
+        {
+            crate.localScale = Vector3.Lerp(start, end, t);
+            t += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+    }
+
+    public void PlaceThemAll(int sizeOfBudget)
+    {
+        //kasse 7 er i 0,0,0 
+        //kasse 6 er i 1.5f,0,0
+        GameObject crate = Instantiate(Crate, transform);
+        ListOfCrates[7, 0] = crate;
+        crate.transform.position = transform.position;
+        crate.transform.rotation = transform.rotation;
+        PlaceThemRepeat(5, crate.transform.position, crate.transform.rotation, false);
+        GameObject crate2 = Instantiate(Crate, transform);
+        ListOfCrates[6, 0] = crate2;
+        crate2.transform.position = crate.transform.position + crate.transform.right * SizeOfCrate.x; 
+        crate2.transform.rotation = transform.rotation;
+        PlaceThemRepeat(5, crate2.transform.position, crate2.transform.rotation, true);
+    }
+     void PlaceThemRepeat(int x, Vector3 prevPos, Quaternion prevRot, bool clockwise)
+    {
+        if(x<1)
+        {
+            return;
+        }
+        else if(clockwise)
+        {
+            GameObject crate = Instantiate(Crate,prevPos,prevRot, transform);
+            ListOfCrates[x, 0] = crate;
+            crate.transform.position += crate.transform.forward * SizeOfCrate.z / 2f + crate.transform.right * SizeOfCrate.x/2f; //first edge of crate
+            crate.transform.rotation *= Quaternion.AngleAxis(-AngleRotate, crate.transform.up); //rotated to new angle
+            crate.transform.position += crate.transform.forward * -SizeOfCrate.z / 2f + crate.transform.right * SizeOfCrate.x / 2f; //back to right position
+            PlaceThemRepeat(x - 1, crate.transform.position, crate.transform.rotation, clockwise);
+        }
+        else
+        {
+            GameObject crate = Instantiate(Crate, prevPos, prevRot, transform);
+            ListOfCrates[13 - x, 0] = crate;
+            crate.transform.position += crate.transform.forward * SizeOfCrate.z / 2f - crate.transform.right * SizeOfCrate.x / 2f; //first edge of crate
+            crate.transform.rotation *= Quaternion.AngleAxis(AngleRotate, crate.transform.up); //rotated to new angle
+            crate.transform.position += crate.transform.forward * -SizeOfCrate.z / 2f - crate.transform.right * SizeOfCrate.x / 2f; //back to right position
+            PlaceThemRepeat(x - 1, crate.transform.position, crate.transform.rotation, clockwise);
+        }
+    }
+    public void PlaceANewRow() //place the rest of all the rows needed
+    {
+        for (int i = 1; i < 13 ; i++) //runs through all the crates
+        {
+            for (int k = 1; k < SizeOfBudget; k++)
+            {
+                ListOfCrates[i,k] = Instantiate(Crate, transform);
+                ListOfCrates[i,k].transform.position = ListOfCrates[i, 0].transform.position + Vector3.down * SizeOfCrate.y * k;
+                ListOfCrates[i, k].transform.rotation = ListOfCrates[i, 0].transform.rotation;
+            }
+        }
+    }
+    int findRowNumber ()
+    {
+        for (int i = 0; i < SizeOfBudget; i++)
+        {
+            if(ListOfCrates[1, i] == null)
+                return i;
+        }
+        return SizeOfBudget;
+    }
+
+}
