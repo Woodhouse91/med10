8642a6c638e204da892b4672bdfc6bd7fbdcc74a
KristofferLH <kristoffer_lh@hotmail.com>
2017-03-15 16:03:37 +0100

    meta

--- Assets/CurrencyMover.cs
+++ Assets/CurrencyMover.cs
@@ -13,4 +13,35 @@ public class CurrencyMover : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    //void spawnMoney(int money) // DET SAMME SOM FRA "AddMoneyToTable" MEN MODIFICERET TIL DET HER SCRIPT
+    //{
+    //    int restMoney = money;
+    //    ModulusMoney(restMoney, 1000, Thousand);
+    //    restMoney %= 1000;
+    //    ModulusMoney(restMoney, 500, FiveHundred);
+    //    restMoney %= 500;
+    //    ModulusMoney(restMoney, 200, TwoHundred);
+    //    restMoney %= 200;
+    //    ModulusMoney(restMoney, 100, OneHundred);
+    //    restMoney %= 100;
+    //    ModulusMoney(restMoney, 50, Fifty);
+    //    restMoney %= 50;
+    //    ModulusMoney(restMoney, 20, Twenty);
+    //    restMoney %= 20;
+    //    ModulusMoney(restMoney, 10, Ten);
+    //    restMoney %= 10;
+    //    ModulusMoney(restMoney, 5, Five);
+    //    restMoney %= 5;
+    //    ModulusMoney(restMoney, 2, Two);
+    //    restMoney %= 2;
+    //    ModulusMoney(restMoney, 1, One);
+    //}
+    //void ModulusMoney(int m, int bill, GameObject go)
+    //{
+    //    for (int i = 0; i < m / bill; i++)
+    //    {
+    //        insideArea(go);
+    //    }
+    //}
 }
