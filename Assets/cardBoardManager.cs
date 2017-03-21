8ee083c6c4ff6d6092108579c9a06eeeccd2573c
KristofferLH <kristoffer_lh@hotmail.com>
2017-03-21 11:29:29 +0100

    cardboard manager fest

---
+++ Assets/cardBoardManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardBoardManager : MonoBehaviour {
+
+    DataHandler DH;
+    List<Transform> CardBoxList;
+    Vector3 initialPos, nextRight,nextUp;
+    public GameObject CardBoxPrefab;
+    List<Sprite> boxIcons;
+    // Use this for initialization
+	void Start () {
+        StartAfterLoad();
+	}
+	void StartAfterLoad()
+    {
+        initialPos = new Vector3(1.5f, 0.28f, 0f); // starten af pyramiden i bunden til venstre
+        nextRight = new Vector3(-0.55f, 0, 0); // den næste der skal stå til højre for den forrige kasse
+        nextUp = new Vector3(-0.275f, 0.51f, 0f); // når man hopper en tak op til næste række
+        CardBoxList = new List<Transform>();
+        boxIcons = new List<Sprite>();
+
+        for (int i = 0; i < Resources.LoadAll("/BoxIcons").Length; i++)
+        {
+            boxIcons.Add((Sprite)Resources.LoadAll("/BoxIcons")[i]);
+        }
+        CreateAllBoxes();
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void CreateAllBoxes()
+    {
+        //int budgetCatCount = DataHandler.BudgetCategories.Count;
+        int budgetCatCount = 1; //test
+        int i = 0;
+        while(budgetCatCount > i)
+        {
+            budgetCatCount -= i;
+            ++i;
+        } // finds the correct number of rows
+
+        //budgetCatCount = DataHandler.BudgetCategories.Count;
+        budgetCatCount = 1; //test
+        for (int h = 0; h < i; h++)
+        {
+            for (int w = 0; w < i - h; w++)
+            {
+                if (budgetCatCount <= 0) 
+                    return;
+                else
+                {
+                    GameObject box = Instantiate(CardBoxPrefab, transform);
+                    box.transform.localPosition = initialPos + nextRight * w + nextUp * h;
+                    box.transform.rotation = transform.rotation;
+                    PaintBox(box,budgetCatCount);
+                    budgetCatCount--;
+                }
+            }
+        }
+    }
+    void PaintBox(GameObject box, int category)
+    {
+        box.GetComponentInChildren<TextMesh>().text = DataHandler.BudgetCategories[category];
+        box.GetComponentInChildren<SpriteRenderer>().sprite = boxIcons[DataHandler.expenseData[category, 0]];
+        //box.GetComponent<BoxBehaviour>().setCategory(DataHandler.expenseData[category, 0]);
+    }
+}
