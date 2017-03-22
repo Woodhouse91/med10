221daab3400eb7c2359eb833be68858f4646f51a
SparNordGit <nskovh11@student.aau.dk>
2017-03-22 09:13:11 +0100

    no message

--- Assets/SteamVR/Extras/OptimizedCameraTex.cs
+++ Assets/SteamVR/Extras/OptimizedCameraTex.cs
@@ -1,5 +1,4 @@
-﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
-using UnityEngine;
+﻿using UnityEngine;
 using System;
 public class OptimizedCameraTex : MonoBehaviour
 {
