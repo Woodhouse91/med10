7419aa8df3974e91efaa15bde3978ff3e427cf2a
KristofferLH <kristoffer_lh@hotmail.com>
2017-03-29 12:42:31 +0200

    5 sekunders reglen gælder også i dette tilfælde

--- Assets/WwisePiano.cs
+++ Assets/WwisePiano.cs
@@ -5,39 +5,7 @@ using UnityEngine;
 
 public class WwisePiano : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        EventManager.OnExcelDataLoaded += StartPiano;
-	}
-
-    private void StartPiano()
-    {
-        print("play");
-        AkSoundEngine.PostEvent("Ambient", gameObject);
-    }
-
-
-    private void Unsub()
-    {
-        EventManager.OnExcelDataLoaded -= StartPiano;
-    }
-    private void OnApplicationQuit()
-    {
-        Unsub();
-    }
-    private void OnDestroy()
-    {
-        Unsub();
-    }
-    private void OnDisable()
-    {
-        Unsub();
-    }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
+	
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.relativeVelocity.magnitude > 1f)
