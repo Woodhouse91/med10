211ed7313c0a4822a1e21b587aa87dc5fb297162
SparNordGit <nskovh11@student.aau.dk>
2017-04-04 17:27:46 +0200

    no message

--- Assets/LuxusSegmentHandler.cs
+++ Assets/LuxusSegmentHandler.cs
@@ -6,35 +6,80 @@ using UnityEngine.UI;
 
 public class LuxusSegmentHandler : MonoBehaviour {
     private static Vector3 offset = new Vector3(0, 0, .175f);
+    private static Vector3 basePos = Vector3.forward * -.56f;
     private static Transform segmentPrefab;
     private static Transform holder, nHolder;
     private static List<Transform> segment;
     private static List<Transform> activeSegments;
-    private static List<List<Transform>> moneyPerCategory;
-    private static List<Transform> moneyInCategory;
-	// Use this for initialization
-	private void Start ()
+    private static float RotAngle = 55f;
+    private static float slideTime = 2f;
+    private static float rotTime = 1.5f;
+    private float moneyMoveTime = 2f;
+    private float moneyH = 0.072f;
+    private bool stillMoving;
+    private static List<List<Transform>> data;
+
+    // Use this for initialization
+    private void Start ()
     {
-        moneyPerCategory = new List<List<Transform>>();
-        moneyInCategory = new List<Transform>();
+        data = new List<List<Transform>>();
         activeSegments = new List<Transform>();
         StaticStart();
+        EventManager.OnBoxAtTable += GenerateTable;
+        EventManager.OnRipTapeSliderDone += MoveMoneyToTable;
 	}
     private static void StaticStart()
     {
         segmentPrefab = Resources.Load<Transform>("LuxusSegment");
         holder = GameObject.Find("Segmentholder").transform;
-        EventManager.OnBoxAtTable += GenerateTable;
         EventManager.OnExcelDataLoaded += PoolSegments;
-        EventManager.OnRipTapeSliderDone += MoveMoneyToTable;
     }
-
-    private static void MoveMoneyToTable()
+    void Unsub()
     {
-        
+        try
+        {
+            EventManager.OnBoxAtTable -= GenerateTable;
+            EventManager.OnRipTapeSliderDone -= MoveMoneyToTable;
+            EventManager.OnExcelDataLoaded -= PoolSegments;
+        }
+        catch { }
+    }
+    private void OnApplicationQuit()
+    {
+        Unsub();
+    }
+    private void OnDisable()
+    {
+        Unsub();
+    }
+    private void OnDestroy()
+    {
+        Unsub();
+    }
+    private void MoveMoneyToTable()
+    {
+        Transform tar;
+        Vector3 down;
+        int i = 0;
+
     }
 
-    private static void GenerateTable(BoxBehaviour s)
+    private IEnumerator DoMovement(Transform obj, Transform tar)
+    {
+        while (stillMoving)
+            yield return null;
+        float t = 0;
+        Vector3 orgPos = obj.position;
+        while (t <= moneyMoveTime)
+        {
+            t += Time.deltaTime;
+            obj.position = Vector3.Lerp(orgPos, tar.position, t/moneyMoveTime);
+            yield return null;
+        }
+        yield break;
+    }
+
+    private void GenerateTable(BoxBehaviour s)
     {
         nHolder = Instantiate(holder);
         for (int x = 0; x < s.CategoryString.Count; ++x)
@@ -43,23 +88,56 @@ public class LuxusSegmentHandler : MonoBehaviour {
             segment.Remove(segment[0]);
             activeSegments[x].SetParent(holder);
             activeSegments[x].localPosition = offset * x;
-            activeSegments[x].GetChild(0).GetChild(0).GetChild(0).GetComponent<Text>().text = s.CategoryString[x];
+            activeSegments[x].localRotation = Quaternion.Euler(90, -180, 0);
+            activeSegments[x].GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMesh>().text = s.CategoryString[x];
+            activeSegments[x].gameObject.SetActive(true);
             buildMoneyList(s.CategoryInt[x]);
+        }
+        print(data.Count);
+        for(int x = 0; x<data.Count; ++x)
+        {
+            print(data[x].Count);
         }
         ExposeTable();
     }
-    public static void ExposeTable()
+    private void ExposeTable()
     {
-
+        StartCoroutine(moveTable());
     }
-    private static IEnumerator moveTable()
+    private IEnumerator moveTable()
     {
+        stillMoving = true;
+        Vector3 orgPos = holder.localPosition;
+        Vector3 tarPos = orgPos + basePos-offset * (activeSegments.Count-1);
+        print(tarPos);
+        print(orgPos);
+        Quaternion orgRot = holder.rotation;
+        Quaternion tarRot = holder.rotation * Quaternion.AngleAxis(RotAngle, -Vector3.forward);
+        float t;
+        #region Slideout
+        t = 0;
+        while (t <= slideTime)
+        {
+            t += Time.deltaTime;
+            holder.transform.localPosition = Vector3.Lerp(orgPos, tarPos, t / slideTime);
+            yield return null;
+        }
+        #endregion
+        #region Rotate
+        t = 0;
+        while(t<= rotTime)
+        {
+            t += Time.deltaTime;
+            holder.rotation = Quaternion.Slerp(orgRot, tarRot, t / rotTime);
+            yield return null;
+        }
+        #endregion
+        stillMoving = false;
         yield break;
     }
     public static void ReleaseTable()
     {
         activeSegments.Clear();
-        moneyPerCategory.Clear();
         nHolder.SetParent(holder.parent);
         nHolder.localPosition = Vector3.zero;
         nHolder.localRotation = Quaternion.identity;
@@ -69,59 +147,74 @@ public class LuxusSegmentHandler : MonoBehaviour {
     }
     private static void buildMoneyList(int cat)
     {
-        int val = DataHandler.expenseData[13 , cat];
-        for(int x = 0; x<val/1000; ++x)
+        List<Transform> res = new List<Transform>();
+        DataHandler.billRef[] myRef = DataHandler.BillsAtCategory_Month[cat];
+        for(int x = 0; x<myRef.Length; ++x)
         {
-            moneyInCategory.Add(MoneyHolder.getCurrency(1000));
+            for (int f1 = 0; f1 < myRef[x]._1; ++f1)
+            {
+                res.Add(MoneyHolder.getCurrency(1));
+            }
         }
-        val = val % 1000;
-        for (int x = 0; x < val / 500; ++x)
+        for (int x = 0; x < myRef.Length; ++x)
         {
-            moneyInCategory.Add(MoneyHolder.getCurrency(500));
+            for (int f2 = 0; f2 < myRef[x]._2; ++f2)
+            {
+                res.Add(MoneyHolder.getCurrency(2));
+            }
         }
-        val = val % 500;
-        for (int x = 0; x < val / 200; ++x)
+        for (int x = 0; x < myRef.Length; ++x)
         {
-            moneyInCategory.Add(MoneyHolder.getCurrency(200));
+            for (int f5 = 0; f5 < myRef[x]._5; ++f5)
+            {
+                res.Add(MoneyHolder.getCurrency(5));
+            }
         }
-        val = val % 200;
-        for (int x = 0; x < val / 100; ++x)
+        for (int x = 0; x < myRef.Length; ++x)
         {
-            moneyInCategory.Add(MoneyHolder.getCurrency(100));
+            for (int f10 = 0; f10 < myRef[x]._10; ++f10)
+            {
+                res.Add(MoneyHolder.getCurrency(10));
+            }
         }
-        val = val % 100;
-        for (int x = 0; x < val / 50; ++x)
+        for (int x = 0; x < myRef.Length; ++x)
         {
-            moneyInCategory.Add(MoneyHolder.getCurrency(50));
+            for (int f20 = 0; f20 < myRef[x]._20; ++f20)
+            {
+                res.Add(MoneyHolder.getCurrency(20));
+            }
         }
-        val = val % 50;
-        for (int x = 0; x < val / 20; ++x)
+        for (int x = 0; x < myRef.Length; ++x)
         {
-            moneyInCategory.Add(MoneyHolder.getCurrency(20));
+            for (int f50 = 0; f50 < myRef[x]._50; ++f50)
+            {
+                res.Add(MoneyHolder.getCurrency(50));
+            }
         }
-        val = val % 20;
-        for (int x = 0; x < val / 10; ++x)
+        for (int x = 0; x < myRef.Length; ++x)
         {
-            moneyInCategory.Add(MoneyHolder.getCurrency(10));
+            for (int f100 = 0; f100 < myRef[x]._100; ++f100)
+            {
+                res.Add(MoneyHolder.getCurrency(100));
+            }
         }
-        val = val % 10;
-        for (int x = 0; x < val / 5; ++x)
+        for (int x = 0; x < myRef.Length; ++x)
         {
-            moneyInCategory.Add(MoneyHolder.getCurrency(5));
+            for (int f200 = 0; f200 < myRef[x]._200; ++f200)
+            {
+                res.Add(MoneyHolder.getCurrency(200));
+            }
         }
-        val = val % 5;
-        for (int x = 0; x < val / 2; ++x)
+        for (int x = 0; x < myRef.Length; ++x)
         {
-            moneyInCategory.Add(MoneyHolder.getCurrency(2));
+            for (int f500 = 0; f500 < myRef[x]._500; ++f500)
+            {
+                res.Add(MoneyHolder.getCurrency(500));
+            }
         }
-        val = val % 2;
-        for (int x = 0; x < val / 1; ++x)
-        {
-            moneyInCategory.Add(MoneyHolder.getCurrency(1));
-        }
-        moneyPerCategory.Add(moneyInCategory);
-        moneyInCategory.Clear();
+        data.Add(res);
     }
+
     private static void PoolSegments()
     {
         if (segment == null)
