16b0c793d9e2aae1706434807dae2e14cf095237
SparNordGit <nskovh11@student.aau.dk>
2017-04-04 11:09:11 +0200

    kris pusser tingene

--- Assets/LuxusSegmentHandler.cs
+++ Assets/LuxusSegmentHandler.cs
@@ -2,29 +2,135 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LuxusSegmentHandler : MonoBehaviour {
-
+    private static Vector3 offset = new Vector3(0, 0, .175f);
+    private static Transform segmentPrefab;
+    private static Transform holder, nHolder;
+    private static List<Transform> segment;
+    private static List<Transform> activeSegments;
+    private static List<List<Transform>> moneyPerCategory;
+    private static List<Transform> moneyInCategory;
 	// Use this for initialization
-	void Start () {
-        EventManager.OnBoxAtTable += GenerateTable;
-	}
-
-    private void GenerateTable(BoxBehaviour s)
+	private void Start ()
     {
+        moneyPerCategory = new List<List<Transform>>();
+        moneyInCategory = new List<Transform>();
+        activeSegments = new List<Transform>();
+        StaticStart();
+	}
+    private static void StaticStart()
+    {
+        segmentPrefab = Resources.Load<Transform>("LuxusSegment");
+        holder = GameObject.Find("Segmentholder").transform;
+        EventManager.OnBoxAtTable += GenerateTable;
+        EventManager.OnExcelDataLoaded += PoolSegments;
+        EventManager.OnRipTapeSliderDone += MoveMoneyToTable;
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    private static void MoveMoneyToTable()
+    {
+        
+    }
+
+    private static void GenerateTable(BoxBehaviour s)
+    {
+        nHolder = Instantiate(holder);
+        for (int x = 0; x < s.CategoryString.Count; ++x)
+        {
+            activeSegments.Add(segment[0]);
+            segment.Remove(segment[0]);
+            activeSegments[x].SetParent(holder);
+            activeSegments[x].localPosition = offset * x;
+            activeSegments[x].GetChild(0).GetChild(0).GetChild(0).GetComponent<Text>().text = s.CategoryString[x];
+            buildMoneyList(s.CategoryInt[x]);
+        }
+        ExposeTable();
+    }
     public static void ExposeTable()
     {
 
     }
+    private static IEnumerator moveTable()
+    {
+        yield break;
+    }
     public static void ReleaseTable()
     {
+        activeSegments.Clear();
+        moneyPerCategory.Clear();
+        nHolder.SetParent(holder.parent);
+        nHolder.localPosition = Vector3.zero;
+        nHolder.localRotation = Quaternion.identity;
 
+
+        holder = nHolder;
+    }
+    private static void buildMoneyList(int cat)
+    {
+        int val = DataHandler.expenseData[13 , cat];
+        for(int x = 0; x<val/1000; ++x)
+        {
+            moneyInCategory.Add(MoneyHolder.getCurrency(1000));
+        }
+        val = val % 1000;
+        for (int x = 0; x < val / 500; ++x)
+        {
+            moneyInCategory.Add(MoneyHolder.getCurrency(500));
+        }
+        val = val % 500;
+        for (int x = 0; x < val / 200; ++x)
+        {
+            moneyInCategory.Add(MoneyHolder.getCurrency(200));
+        }
+        val = val % 200;
+        for (int x = 0; x < val / 100; ++x)
+        {
+            moneyInCategory.Add(MoneyHolder.getCurrency(100));
+        }
+        val = val % 100;
+        for (int x = 0; x < val / 50; ++x)
+        {
+            moneyInCategory.Add(MoneyHolder.getCurrency(50));
+        }
+        val = val % 50;
+        for (int x = 0; x < val / 20; ++x)
+        {
+            moneyInCategory.Add(MoneyHolder.getCurrency(20));
+        }
+        val = val % 20;
+        for (int x = 0; x < val / 10; ++x)
+        {
+            moneyInCategory.Add(MoneyHolder.getCurrency(10));
+        }
+        val = val % 10;
+        for (int x = 0; x < val / 5; ++x)
+        {
+            moneyInCategory.Add(MoneyHolder.getCurrency(5));
+        }
+        val = val % 5;
+        for (int x = 0; x < val / 2; ++x)
+        {
+            moneyInCategory.Add(MoneyHolder.getCurrency(2));
+        }
+        val = val % 2;
+        for (int x = 0; x < val / 1; ++x)
+        {
+            moneyInCategory.Add(MoneyHolder.getCurrency(1));
+        }
+        moneyPerCategory.Add(moneyInCategory);
+        moneyInCategory.Clear();
+    }
+    private static void PoolSegments()
+    {
+        if (segment == null)
+            segment = new List<Transform>();
+        for(int x = 0; x< DataHandler.BudgetCategories.Count; ++x)
+        {
+            segment.Add(Instantiate(segmentPrefab));
+            segment[x].gameObject.SetActive(false);
+        }
     }
 
 }
--- Assets/Scripts/MoneyHolder.cs
+++ Assets/Scripts/MoneyHolder.cs
@@ -6,6 +6,7 @@ public class MoneyHolder : MonoBehaviour
 {
     [SerializeField]
     private int CurrencySpawnedPerFrame;
+    public static List<Transform> _1000, _500, _200, _100, _50, _20, _10, _5, _2, _1;
     public static List<GameObject> allCurrency;
     DataHandler.TotalBills tBills;
     private static GameObject pref1000, pref500, pref200, pref100, pref50, pref20, pref10, pref5, pref2, pref1;
@@ -35,6 +36,9 @@ public class MoneyHolder : MonoBehaviour
         pref2 = Resources.Load<GameObject>("_2");
         pref1 = Resources.Load<GameObject>("_1");
         allCurrency = new List<GameObject>();
+        _1000 = new List<Transform>(); _500 = new List<Transform>(); _200 = new List<Transform>(); _100 = new List<Transform>();
+        _50 = new List<Transform>(); _20 = new List<Transform>(); _10 = new List<Transform>(); _5 = new List<Transform>();
+        _2 = new List<Transform>(); _1 = new List<Transform>();
         EventManager.OnExcelDataLoaded += PoolMoney;
     }
     private void PoolMoney()
@@ -115,6 +119,26 @@ public class MoneyHolder : MonoBehaviour
         while (i < count)
         {
             allCurrency.Add(Instantiate(go));
+            if (go == pref1000)
+                _1000.Add(allCurrency[i].transform);
+            else if (go == pref500)
+                _500.Add(allCurrency[i].transform);
+            else if (go == pref200)
+                _200.Add(allCurrency[i].transform);
+            else if (go == pref100)
+                _100.Add(allCurrency[i].transform);
+            else if (go == pref50)
+                _50.Add(allCurrency[i].transform);
+            else if (go == pref20)
+                _20.Add(allCurrency[i].transform);
+            else if (go == pref10)
+                _10.Add(allCurrency[i].transform);
+            else if (go == pref5)
+                _5.Add(allCurrency[i].transform);
+            else if (go == pref2)
+                _2.Add(allCurrency[i].transform);
+            else if (go == pref1)
+                _1.Add(allCurrency[i].transform);
             ++curSpawned;
             i++;
             if (i % CurrencySpawnedPerFrame == 0)
@@ -122,5 +146,53 @@ public class MoneyHolder : MonoBehaviour
         }
         spawning = false;
         yield break;
+    }
+    public static Transform getCurrency(int currency)
+    {
+        Transform res;
+        switch (currency)
+        {
+            case 1000:
+                res = _1000[0];
+                _1000.Remove(_1000[0]);
+                return res;
+            case 500:
+                res = _500[0];
+                _500.Remove(_500[0]);
+                return res;
+            case 200:
+                res = _200[0];
+                _200.Remove(_200[0]);
+                return res;
+            case 100:
+                res = _100[0];
+                _100.Remove(_100[0]);
+                return res;
+            case 50:
+                res = _50[0];
+                _1000.Remove(_50[0]);
+                return res;
+            case 20:
+                res = _20[0];
+                _20.Remove(_20[0]);
+                return res;
+            case 10:
+                res = _10[0];
+                _10.Remove(_10[0]);
+                return res;
+            case 5:
+                res = _5[0];
+                _5.Remove(_5[0]);
+                return res;
+            case 2:
+                res = _2[0];
+                _2.Remove(_5[0]);
+                return res;
+            case 1:
+                res = _1[0];
+                _1.Remove(_1[0]);
+                return res;
+        }
+        return null;
     }
 }
