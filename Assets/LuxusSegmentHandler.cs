e91b8a4168db7de5833a245bd3ca87f4580ab4f8
SparNordGit <nskovh11@student.aau.dk>
2017-04-05 15:55:47 +0200

    Luxusbord TODO - bugger hvis tape er trukket før det er "ude"

--- Assets/LuxusSegmentHandler.cs
+++ Assets/LuxusSegmentHandler.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class LuxusSegmentHandler : MonoBehaviour {
     private static Vector3 offset = new Vector3(0, 0, .175f);
+    private static float scaledOffset = 0;
     private static Vector3 basePos = Vector3.forward * -.56f;
     private static Transform segmentPrefab;
     private static Transform holder, nHolder;
@@ -16,13 +17,17 @@ public class LuxusSegmentHandler : MonoBehaviour {
     private static float rotTime = 1.5f;
     private float moneyMoveTime = 2f;
     private float moneyH = 0.072f;
+    private float moneyW = 0.165f;
+    private int billsPerColumn = 25;
     private bool stillMoving;
-    private static List<List<Transform>> data;
+    private static List<List<Transform>> bills;
+    private static List<List<Transform>> coins;
 
     // Use this for initialization
     private void Start ()
     {
-        data = new List<List<Transform>>();
+        bills = new List<List<Transform>>();
+        coins = new List<List<Transform>>();
         activeSegments = new List<Transform>();
         StaticStart();
         EventManager.OnBoxAtTable += GenerateTable;
@@ -60,20 +65,41 @@ public class LuxusSegmentHandler : MonoBehaviour {
     {
         Transform tar;
         Vector3 down;
-        int i = 0;
-
+        for(int x = 0; x<activeSegments.Count; ++x)
+        {
+            List<Vector3> offset = new List<Vector3>();
+            tar = activeSegments[x].GetChild(1).GetChild(0);
+            down = -(activeSegments[x].GetChild(1).GetChild(0).up*moneyH/5f);
+            int cols = bills[x].Count / billsPerColumn+1;
+            int downOffset = 0;
+            for (int k = 0; k<cols; ++k)
+            {
+                offset.Add(activeSegments[x].GetChild(1).GetChild(0).right * (moneyW * (((-cols/2f) +k)+.5f)));
+            }
+            for (int y = 0; y < bills[x].Count; ++y)
+            {
+                StartCoroutine(DoMovement(bills[x][y], tar, (down * downOffset) + offset[y % offset.Count]));
+                if (cols == 1)
+                    ++downOffset;
+                else if (y % cols == 1)
+                    ++downOffset;
+            }
+        }
     }
 
-    private IEnumerator DoMovement(Transform obj, Transform tar)
+    private IEnumerator DoMovement(Transform obj, Transform tar, Vector3 offset)
     {
         while (stillMoving)
             yield return null;
         float t = 0;
         Vector3 orgPos = obj.position;
-        while (t <= moneyMoveTime)
+        Quaternion orgRot = obj.rotation;
+        Quaternion tarRot = tar.rotation*Quaternion.AngleAxis(15, -Vector3.right);
+        while (t <= 1)
         {
-            t += Time.deltaTime;
-            obj.position = Vector3.Lerp(orgPos, tar.position, t/moneyMoveTime);
+            t += Time.deltaTime/moneyMoveTime;
+            obj.position = Vector3.Lerp(orgPos, tar.position+offset, t);
+            obj.rotation = Quaternion.Lerp(orgRot, tarRot, t);
             yield return null;
         }
         yield break;
@@ -82,23 +108,31 @@ public class LuxusSegmentHandler : MonoBehaviour {
     private void GenerateTable(BoxBehaviour s)
     {
         nHolder = Instantiate(holder);
+        int scaleFactor = 0;
         for (int x = 0; x < s.CategoryString.Count; ++x)
         {
             activeSegments.Add(segment[0]);
             segment.Remove(segment[0]);
             activeSegments[x].SetParent(holder);
-            activeSegments[x].localPosition = offset * x;
+            scaleFactor = buildMoneyList(s.CategoryInt[x])/billsPerColumn;
+            activeSegments[x].localPosition = offset * x + offset * scaledOffset;
+            scaledOffset += (scaleFactor / 2f);
+            activeSegments[x].localScale += Vector3.up*scaleFactor;
             activeSegments[x].localRotation = Quaternion.Euler(90, -180, 0);
-            activeSegments[x].GetChild(0).GetChild(0).GetChild(0).GetComponent<TextMesh>().text = s.CategoryString[x];
+            activeSegments[x].GetChild(0).GetChild(0).GetComponent<TextMesh>().text = s.CategoryString[x];
+            activeSegments[x].GetChild(0).GetChild(1).GetComponent<TextMesh>().text = formatCurrency(DataHandler.expenseData[13, s.CategoryInt[x]]);
+            Vector3 texScale = activeSegments[x].GetChild(0).GetChild(0).localScale;
+            texScale.x /= (scaleFactor+1);
+            activeSegments[x].GetChild(0).GetChild(0).localScale = texScale;
+            activeSegments[x].GetChild(0).GetChild(1).localScale = texScale;
             activeSegments[x].gameObject.SetActive(true);
-            buildMoneyList(s.CategoryInt[x]);
-        }
-        print(data.Count);
-        for(int x = 0; x<data.Count; ++x)
-        {
-            print(data[x].Count);
         }
         ExposeTable();
+    }
+    private string formatCurrency(int val)
+    {
+        string res = val.ToString();
+        return res;
     }
     private void ExposeTable()
     {
@@ -108,9 +142,7 @@ public class LuxusSegmentHandler : MonoBehaviour {
     {
         stillMoving = true;
         Vector3 orgPos = holder.localPosition;
-        Vector3 tarPos = orgPos + basePos-offset * (activeSegments.Count-1);
-        print(tarPos);
-        print(orgPos);
+        Vector3 tarPos = orgPos + basePos-offset * (activeSegments.Count-1)-offset*scaledOffset;
         Quaternion orgRot = holder.rotation;
         Quaternion tarRot = holder.rotation * Quaternion.AngleAxis(RotAngle, -Vector3.forward);
         float t;
@@ -145,74 +177,86 @@ public class LuxusSegmentHandler : MonoBehaviour {
 
         holder = nHolder;
     }
-    private static void buildMoneyList(int cat)
+    private static int buildMoneyList(int cat)
     {
-        List<Transform> res = new List<Transform>();
+        List<Transform> resBills = new List<Transform>();
+        List<Transform> resCoins = new List<Transform>();
         DataHandler.billRef[] myRef = DataHandler.BillsAtCategory_Month[cat];
-        for(int x = 0; x<myRef.Length; ++x)
-        {
-            for (int f1 = 0; f1 < myRef[x]._1; ++f1)
-            {
-                res.Add(MoneyHolder.getCurrency(1));
-            }
-        }
         for (int x = 0; x < myRef.Length; ++x)
         {
-            for (int f2 = 0; f2 < myRef[x]._2; ++f2)
+            for (int f1000 = 0; f1000 < myRef[x]._1000; ++f1000)
             {
-                res.Add(MoneyHolder.getCurrency(2));
-            }
-        }
-        for (int x = 0; x < myRef.Length; ++x)
-        {
-            for (int f5 = 0; f5 < myRef[x]._5; ++f5)
-            {
-                res.Add(MoneyHolder.getCurrency(5));
-            }
-        }
-        for (int x = 0; x < myRef.Length; ++x)
-        {
-            for (int f10 = 0; f10 < myRef[x]._10; ++f10)
-            {
-                res.Add(MoneyHolder.getCurrency(10));
-            }
-        }
-        for (int x = 0; x < myRef.Length; ++x)
-        {
-            for (int f20 = 0; f20 < myRef[x]._20; ++f20)
-            {
-                res.Add(MoneyHolder.getCurrency(20));
-            }
-        }
-        for (int x = 0; x < myRef.Length; ++x)
-        {
-            for (int f50 = 0; f50 < myRef[x]._50; ++f50)
-            {
-                res.Add(MoneyHolder.getCurrency(50));
-            }
-        }
-        for (int x = 0; x < myRef.Length; ++x)
-        {
-            for (int f100 = 0; f100 < myRef[x]._100; ++f100)
-            {
-                res.Add(MoneyHolder.getCurrency(100));
-            }
-        }
-        for (int x = 0; x < myRef.Length; ++x)
-        {
-            for (int f200 = 0; f200 < myRef[x]._200; ++f200)
-            {
-                res.Add(MoneyHolder.getCurrency(200));
+                resBills.Add(MoneyHolder.getCurrency(1000));
             }
         }
         for (int x = 0; x < myRef.Length; ++x)
         {
             for (int f500 = 0; f500 < myRef[x]._500; ++f500)
             {
-                res.Add(MoneyHolder.getCurrency(500));
+                resBills.Add(MoneyHolder.getCurrency(500));
             }
         }
-        data.Add(res);
+        for (int x = 0; x < myRef.Length; ++x)
+        {
+            for (int f200 = 0; f200 < myRef[x]._200; ++f200)
+            {
+                resBills.Add(MoneyHolder.getCurrency(200));
+            }
+        }
+        for (int x = 0; x < myRef.Length; ++x)
+        {
+            for (int f100 = 0; f100 < myRef[x]._100; ++f100)
+            {
+                resBills.Add(MoneyHolder.getCurrency(100));
+            }
+        }
+        for (int x = 0; x < myRef.Length; ++x)
+        {
+            for (int f50 = 0; f50 < myRef[x]._50; ++f50)
+            {
+                resBills.Add(MoneyHolder.getCurrency(50));
+            }
+        }
+        for (int x = 0; x < myRef.Length; ++x)
+        {
+            for (int f20 = 0; f20 < myRef[x]._20; ++f20)
+            {
+                resCoins.Add(MoneyHolder.getCurrency(20));
+            }
+        }
+        for (int x = 0; x < myRef.Length; ++x)
+        {
+            for (int f10 = 0; f10 < myRef[x]._10; ++f10)
+            {
+                resCoins.Add(MoneyHolder.getCurrency(10));
+            }
+        }
+        for (int x = 0; x < myRef.Length; ++x)
+        {
+            for (int f5 = 0; f5 < myRef[x]._5; ++f5)
+            {
+                resCoins.Add(MoneyHolder.getCurrency(5));
+            }
+        }
+        for (int x = 0; x < myRef.Length; ++x)
+        {
+            for (int f2 = 0; f2 < myRef[x]._2; ++f2)
+            {
+                resCoins.Add(MoneyHolder.getCurrency(2));
+            }
+        }
+        for(int x = 0; x<myRef.Length; ++x)
+        {
+            for (int f1 = 0; f1 < myRef[x]._1; ++f1)
+            {
+                resCoins.Add(MoneyHolder.getCurrency(1));
+            }
+        }
+        if(resCoins.Count>0)
+            coins.Add(resCoins);
+        if(resBills.Count>0)
+            bills.Add(resBills);
+        return resBills.Count;
     }
 
     private static void PoolSegments()
