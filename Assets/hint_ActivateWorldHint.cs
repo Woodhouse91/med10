3d739f51ce7271d1c8f6f1ded2df38d6be24c6cb
SparNordGit <nskovh11@student.aau.dk>
2017-04-28 08:44:25 +0200

    hints so far

---
+++ Assets/hint_ActivateWorldHint.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hint_ActivateWorldHint : MonoBehaviour {
+    [SerializeField]
+    private Transform myHint, myHintPosActive, myHintPosDeactive;
+
+    private bool activating = false;
+    private float activateTimer = 1f;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(deactivate());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Activate(bool b)
+    {
+        print("activating");
+        if (b && !activating)
+            StartCoroutine(activate());
+        if (!b && activating)
+            StartCoroutine(deactivate());
+    }
+    private IEnumerator deactivate()
+    {
+        activating = false;
+        Vector3 orgPos = myHint.position;
+        Quaternion orgRot = myHint.rotation;
+        Vector3 tarPos = myHintPosDeactive.position;
+        Quaternion tarRot = myHintPosDeactive.rotation;
+        Vector3 orgScale = myHint.localScale;
+        Vector3 tarScale = Vector3.zero;
+        float t = 0;
+        while (!activating && t < 1)
+        {
+            t += Time.deltaTime / activateTimer;
+            myHint.position = Vector3.Lerp(orgPos, tarPos, t);
+            myHint.rotation = Quaternion.Lerp(orgRot, tarRot, t);
+            myHint.localScale = Vector3.Lerp(orgScale, tarScale, t);
+            yield return null;
+        }
+        yield break;
+    }
+    private IEnumerator activate()
+    {
+        activating = true;
+        Vector3 orgPos = myHint.position;
+        Quaternion orgRot = myHint.rotation;
+        Vector3 tarPos = myHintPosActive.position;
+        Quaternion tarRot = myHintPosActive.rotation;
+        Vector3 orgScale = myHint.localScale;
+        Vector3 tarScale = Vector3.one;
+        float t = 0;
+        while(activating && t < 1)
+        {
+            t += Time.deltaTime / activateTimer;
+            myHint.position = Vector3.Lerp(orgPos, tarPos, t);
+            myHint.rotation = Quaternion.Lerp(orgRot, tarRot, t);
+            myHint.localScale = Vector3.Lerp(orgScale, tarScale, t);
+            yield return null;
+        }
+        yield break;
+    }
+}
---
+++ Assets/hint_Raycaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hint_Raycaster : MonoBehaviour {
+    RaycastHit hit;
+    private Transform myhit;
+    private int hintLayer = 1;
+	// Use this for initialization
+	void Start () {
+        hintLayer = 1 << LayerMask.NameToLayer("hintlayer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, hintLayer))
+        {
+            if (myhit == null || hit.transform!=myhit){
+                if (myhit != null)
+                    myhit.GetComponent<hint_ActivateWorldHint>().Activate(false);
+                myhit = hit.transform;
+                myhit.GetComponent<hint_ActivateWorldHint>().Activate(true);
+            }
+        }
+        else if(myhit!=null)
+        {
+            myhit.GetComponent<hint_ActivateWorldHint>().Activate(false);
+            myhit = null;
+        }
+
+	}
+}
