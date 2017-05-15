3aeb3fdc61d156112809652a97dbd5b77be91130
SparNordGit <nskovh11@student.aau.dk>
2017-05-15 09:50:49 +0200

    no message

--- Assets/ViveController.cs
+++ Assets/ViveController.cs
@@ -7,13 +7,24 @@ public class ViveController : MonoBehaviour {
     {
         get
         {
-            return SteamVR_Controller.Input(3);
+            return SteamVR_Controller.Input(x);
         }
     }
 
-    
+    private int x = 1;
     private void Update()
     {
+
+        if (Input.GetKey(KeyCode.Alpha1))
+            x = 1;
+        if (Input.GetKey(KeyCode.Alpha2))
+            x = 2;
+        if (Input.GetKey(KeyCode.Alpha3))
+            x = 3;
+        if (Input.GetKey(KeyCode.Alpha4))
+            x = 4;
+
+
         transform.position = controller.transform.pos;
         transform.rotation = controller.transform.rot;
     }
